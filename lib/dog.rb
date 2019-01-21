bfb60575040a155e311f8741abee50f9c5a62c38
Ryan Brady <ryanbrady369@gmail.com>
2019-01-21 22:44:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
     @breed = breed
   end
 
-  def attributes
+  def self.create_table
     DB[:conn].execute("CREATE TABLE IF NOT EXISTS dogs
                         id INTEGER PRIMARY KEY,
                         name TEXT,

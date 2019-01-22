cb0cf494b5606358cdb0c33d7a7995c7639f5525
Ryan Brady <ryanbrady369@gmail.com>
2019-01-22 00:09:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,9 +37,20 @@ class Dog
   end
 
   def self.find_by_id(id)
-    DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id).map {|row| self.new_from_db(row)}.first
+    DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id).map {|row| self.new_from_db(row)}
   end
 
-  #def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first
+
+    if dog 
+      new_dog = self.new_from_db(dog)
+    else 
+      new_dog = self.create(name: name, breed: breed)
+    end 
+
+    new_dog
+  end 
+    
 
 end

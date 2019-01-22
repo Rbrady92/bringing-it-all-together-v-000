3b1936aad301b3ed328742274f960934c54707b0
Ryan Brady <ryanbrady369@gmail.com>
2019-01-22 01:03:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,11 +48,11 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first
 
-    if dog 
+    if dog
       new_dog = self.new_from_db(dog)
-    else 
+    else
       new_dog = self.create(name: name, breed: breed)
-    end 
+    end
 
     new_dog
   end
